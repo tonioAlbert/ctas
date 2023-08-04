ef8c99f0d14942fda991c6e4cbeb825e94b60365
= <ga.randrianarivelo@gmail.com>
2023-08-04 19:39:59 +0300

    Modification page test encore en home

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -2,15 +2,12 @@
 
 namespace App\Controller;
 
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 Class HomeController extends AbstractController
 {
-
 
 
     #[Route('/', name: 'app.home')]
